a134c65e19a549f102c5820f70dc2add460f978b
Shadi Akiki <shadiakiki1986@gmail.com>
2017-09-20 12:33:07 +0300

    add app models. Closes #1

---
+++ app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from . import models
+
+admin.site.register(models.Currency)
+admin.site.register(models.Nationality)
+admin.site.register(models.SecurityShare)
+admin.site.register(models.SecurityOption)
+admin.site.register(models.SecurityBond)
+admin.site.register(models.SecurityFutures)
+
+
---
+++ app/models.py
@@ -0,0 +1,100 @@
+from __future__ import unicode_literals
+#from django.conf import settings
+from django.db import models
+
+class Currency(models.Model):
+
+    curr = models.CharField(max_length=20)
+    currname = models.CharField(max_length=20)
+    def __str__(self):
+        return self.curr
+    class Meta:
+      verbose_name_plural = "currencies"
+
+class Nationality(models.Model):
+
+    nationality = models.CharField(max_length=20)
+    nationality_name = models.CharField(max_length=20 )
+    def __str__(self):
+        return self.nationality
+    class Meta:
+      verbose_name_plural = "nationalities"
+
+class Security(models.Model):
+
+    code = models.CharField(max_length=20)
+    circular = models.CharField(max_length=200)
+    bank_reference = models.CharField(max_length=200)
+    designation = models.CharField(max_length=200)
+    symbol = models.CharField(max_length=200)
+    currency = models.ForeignKey(Currency)
+    subtype = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
+    trading_category = models.CharField(max_length=200)
+    nature = models.CharField(max_length=200)
+    trading_center = models.CharField(max_length=200)
+    nationality = models.ForeignKey(Nationality, null=True, blank=True)
+    quotation_place = models.CharField(max_length=200)
+    deposit_place = models.CharField(max_length=200)
+    ratelist = models.CharField(max_length=200)
+    asset_allocation = models.CharField(max_length=200)
+    group_for_ledgers = models.CharField(max_length=200)
+    general_ledger = models.CharField(max_length=200)
+    provider_code = models.CharField(max_length=200)
+    provider_ratelist = models.CharField(max_length=200)
+    monitoring_type = models.CharField(max_length=200)
+    multiplier_fFor_online_prices = models.CharField(max_length=200)
+    isin = models.CharField(max_length=200)
+    fixing = models.BooleanField(default=False)
+    fix1 = models.CharField(max_length=200)
+    fix2 = models.CharField(max_length=200)
+    
+    class Meta:
+      abstract = True
+
+    def __str__(self):
+       return self.code
+
+#Multi Table Inheritance
+#https://godjango.com/blog/django-abstract-base-class-multi-table-inheritance/   
+class SecurityShare (Security):
+
+    shareholder_number = models.BooleanField(default=False)
+    show = models.BooleanField(default=False)
+
+    def __str__(self):
+       return self.code
+      
+class SecurityOption(Security):
+
+    maturity_date = models.CharField(max_length=200)
+    underlying_code = models.CharField(max_length=200)
+    strike_place = models.CharField(max_length=200)
+
+    def __str__(self):
+       return self.code
+
+
+class SecurityBond(Security):
+
+    moody = models.CharField(max_length=200)
+    fitch = models.CharField(max_length=200)
+    s_and_p = models.CharField(max_length=200)
+    
+    def __str__(self):
+       return self.code
+
+
+class SecurityFutures(Security):
+
+    maturity_date = models.CharField(max_length=200)
+    underlying_code = models.CharField(max_length=200)
+    number_of_units = models.CharField(max_length=200)
+    first_notice_date = models.CharField(max_length=200)
+    shareholder_number = models.BooleanField(default=False)
+    show = models.BooleanField(default=False)
+    def __str__(self):
+       return self.code
+    class Meta:
+      verbose_name_plural = "security futures"
+
